7e806f14712343cf972f3a45027fab9a7033f4d4
Miky <vyasmiky@gmail.com>
2026-01-13 19:36:10 -0500

    disabled tenor api

--- image_handler.py
+++ image_handler.py
@@ -4,22 +4,7 @@ import httpx
 from pathlib import Path
 from urllib.parse import urlparse
 from typing import Optional
-import os
-import msgspec
 from abc import ABC
-
-
-class Media(msgspec.Struct):
-    url: str
-
-
-class Response(msgspec.Struct):
-    media_formats: dict[str, Media]
-
-
-class TenorAPIResponse(msgspec.Struct):
-    results: list[Response]
-
 
 @define
 class DiscordImage(ABC):
@@ -94,40 +79,7 @@ async def create_image_class(
         if "tenor.com" in link:
             if link.endswith(".mp4") or link.endswith(".webm"):
                 raise ValueError("link must redirect to a gif")
-            tenor_link = await tenorsearch(link)
-            link = tenor_link
+            else:
+                raise ValueError("Tenor links are not supported. Please provide a direct link to the image")
 
         return UrlImage(link=link, filetype=filetype)
-
-
-async def tenorsearch(url: str) -> str:
-    """Searches for gif using tenor API"""
-    # set the apikey and limit
-    apikey = os.getenv("TENOR_TOKEN")
-    if apikey is None:
-        print("Tenor API key is not set")
-        raise ValueError("tenor API key missing")
-
-    ckey = "MemeBot"
-    id = url.split("-")[-1]
-
-    async with httpx.AsyncClient() as client:
-        r = await client.get(
-            "https://tenor.googleapis.com/v2/posts",
-            params={
-                "key": apikey,
-                "ids": id,
-                "client_key": ckey,
-                "media_filter": "gif",
-            },
-        )
-
-    if r.status_code == 200:
-        decoder = msgspec.json.Decoder(type=TenorAPIResponse)
-        # load the GIFs using the urls for the smaller GIF sizes
-        # response = r.json()
-        response = decoder.decode(r.content)
-        tenor_url = response.results[0].media_formats["gif"].url
-        return tenor_url
-    else:
-        raise ValueError("Non 200 status code for tenor api")
